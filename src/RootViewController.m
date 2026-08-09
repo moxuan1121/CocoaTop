22dc867b88864fc3315c79f49fcf652778c71ef6
moxuan1121 <99877708+moxuan1121@users.noreply.github.com>
2026-08-09 19:20:17 +0900

    Update RootViewController.m

--- src/RootViewController.m
+++ src/RootViewController.m
@@ -356,7 +356,7 @@
 	}
 	[self.navigationController.navigationBar layoutIfNeeded];
 	[self layoutStatusLabel];
-	statusLabel.text = [NSString stringWithFormat:@"进程：%lu   可用：%.1f MB   CPU：%.1f%%",
+	statusLabel.text = [NSString stringWithFormat:@"当前进程数:%lu   可用运存:%.1f MB   CPU占用:%.1f%%",
 		(unsigned long)procs.count, (float)procs.memFree / 1024 / 1024, (float)procs.totalCpu / 10];
 }
 
