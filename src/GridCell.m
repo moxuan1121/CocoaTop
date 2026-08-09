ffd78ec3f49f07f016235adf0305ef34af2fcb16
moxuan1121 <99877708+moxuan1121@users.noreply.github.com>
2026-08-09 16:43:54 +0900

    Fix summary style source formatting

--- src/GridCell.m
+++ src/GridCell.m
@@ -248,7 +248,9 @@
 		[self.labels addObject:label];
 		label.textAlignment = footer && col.getSummary ? col.align : NSTextAlignmentCenter;
 		label.font = footer ? [UIFont systemFontOfSize:16.0] : [UIFont boldSystemFontOfSize:16.0];
-		label.adjustsFontSizeToFitWidth = YES;\n\t\tlabel.minimumScaleFactor = 0.6;\n\t\tlabel.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+		label.adjustsFontSizeToFitWidth = YES;
+\t\tlabel.minimumScaleFactor = 0.6;
+\t\tlabel.autoresizingMask = UIViewAutoresizingFlexibleHeight;
 		label.text = footer ? @"-" : col.name;
         if (@available(iOS 13, *)) {
             label.textColor = [UIColor labelColor];
